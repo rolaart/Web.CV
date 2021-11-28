a08ee26f7ea307fef49fc95df10199a2a9ed68fa
Ivelina Balcheva <ivab1414@gmail.com>
2021-11-28 19:22:40 +0200

    Comments work

---
+++ JS/comments.js
@@ -0,0 +1,21 @@
+var comment_btn = document.getElementById('#comment__btn');
+var name_area = document.querySelector('.name__textarea');
+var comment_area = document.querySelector('.comment__textarea');
+
+var insertComment = (event) => {
+  event.preventDefault();
+  let name = name_area.value;
+  let comment = comment_area.value;
+  name_elem = document.createElement('div');
+  comment_elem = document.createElement('div');
+  name_elem.classList.add('.name');
+  comment_elem.classList.add('.comment');
+  name_elem.innerText = name;
+  comment_elem.innerText = comment;
+  let comments_elem = document.querySelector('.comments');
+  comments_elem.appendChild(name_elem);
+  comments_elem.appendChild(comment_elem);
+  //console.log("some text here");
+};
+
+comment_btn.addEventListener('click', insertComment)
