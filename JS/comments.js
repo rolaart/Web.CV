e90624f583ed7f77cfed0cc9517adbe8522ef94e
Ivelina Balcheva <ivab1414@gmail.com>
2021-11-28 20:16:58 +0200

    Show this to Lucho

--- JS/comments.js
+++ JS/comments.js
@@ -1,13 +1,14 @@
 var comment_btn = document.getElementById('#comment__btn');
 var name_area = document.querySelector('.name__textarea');
 var comment_area = document.querySelector('.comment__textarea');
+var clear = document.getElementById('clear')
 
 var insertComment = (event) => {
   event.preventDefault();
   let name = name_area.value;
   let comment = comment_area.value;
-  name_elem = document.createElement('div');
-  comment_elem = document.createElement('div');
+  name_elem = document.createElement('h2');
+  comment_elem = document.createElement('h3');
   name_elem.classList.add('.name');
   comment_elem.classList.add('.comment');
   name_elem.innerText = name;
@@ -17,5 +18,11 @@ var insertComment = (event) => {
   comments_elem.appendChild(comment_elem);
   //console.log("some text here");
 };
+
+clear.onclick = () => {
+    event.preventDefault();
+    name_area.value = '';
+    comment_area.value = '';
+}
 
 comment_btn.addEventListener('click', insertComment)
